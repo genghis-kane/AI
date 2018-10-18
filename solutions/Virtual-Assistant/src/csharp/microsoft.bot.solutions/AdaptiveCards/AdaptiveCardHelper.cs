4976e922a729784a8edf81c46de309dea4a36206
Qiong Wu <qiowu@microsoft.com>
2018-10-18 17:20:42 +0800

    email adaptive card show bug

--- solutions/Virtual-Assistant/src/csharp/microsoft.bot.solutions/AdaptiveCards/AdaptiveCardHelper.cs
+++ solutions/Virtual-Assistant/src/csharp/microsoft.bot.solutions/AdaptiveCards/AdaptiveCardHelper.cs
@@ -42,7 +42,10 @@ namespace Microsoft.Bot.Solutions.AdaptiveCards
                 var escapedTokens = new StringDictionary();
                 foreach (string key in tokens.Keys)
                 {
-                    escapedTokens.Add(key, tokens[key]?.Replace("\"", "\\\""));
+                    var formattedToken = tokens[key]?.Replace("\"", "\\\"");
+                    formattedToken = formattedToken?.Replace("\\", "\\\\");
+                    formattedToken = formattedToken?.Replace("\'", "\\\'");
+                    escapedTokens.Add(key, formattedToken);
                 }
 
                 jsonCard = CardTokensRegex.Replace(jsonCard, match => escapedTokens[match.Groups[1].Value]);
